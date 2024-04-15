365663830aeb37e3268e2efb30a546d0955013e4
Sebastián García <johancs.mm@gmail.com>
2024-04-14 21:58:50 -0500

    Update function sum (#10)

---
+++ src/sum.rs
@@ -0,0 +1,28 @@
+#[derive(Debug)]
+pub struct Sum {}
+
+impl Sum {
+  pub fn new(nums: Vec<i32>) -> i32 {
+    let mut total = 0;
+
+    for num in nums.iter() {
+      total += num
+    }
+
+    return total;
+  }
+}
+
+#[test]
+fn test_sum() {
+  let result = Sum::new(vec![2, 2]);
+  
+  assert_eq!(result, 4);
+}
+
+#[test]
+fn test_sum_error() {
+    let result = Sum::new(vec![2, 2]);
+    
+    assert_ne!(result, 5);
+}
