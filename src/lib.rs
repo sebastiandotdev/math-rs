1fb4741c7b8e95b1d738e3961a013c0c1587ea43
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-04-14 19:04:47 -0500

    Update branch master

---
+++ src/lib.rs
@@ -0,0 +1,8 @@
+// Copyright 2024 Developers castrogarciajs.
+//
+// Licensed under the MIT License, the MIT license
+// <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
+// option. This file may not be copied, modified, or distributed
+// except according to those terms.
+
+mod utils;
