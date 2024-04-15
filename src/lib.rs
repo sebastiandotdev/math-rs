e7ec9067ce928c2af3ab27020dee42bf84dad116
johancs.mm@gmail.com <johancs.mm@gmail.com>
2024-04-14 21:58:18 -0500

    Update function sum

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,9 @@
-// Copyright 2024 Developers castrogarciajs.
+// Copyright 2024 Developer Sebastian Garcia.
 //
 // Licensed under the MIT License, the MIT license
 // <LICENSE-MIT or https://opensource.org/licenses/MIT>, at your
 // option. This file may not be copied, modified, or distributed
 // except according to those terms.
 
-mod utils;
+mod sum;
+pub use sum::Sum;
